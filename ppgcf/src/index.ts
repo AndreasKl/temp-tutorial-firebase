45fa4bd0755b21ba850edfa90eb228f60ab4dbf1
Christian <christian.oehne@rewe-digital.com>
2022-11-10 10:58:36 +0100

    add basic function structure

--- ppgcf/src/index.ts
+++ ppgcf/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -13,3 +14,16 @@ export const helloCallableWorld = functions.https.onCall((data, context) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   return "Hello from Firebase!";
 });
+
+
+export const sendSendingConfirmationInToChat =
+ functions.https.onCall((data, context) => {
+   const messageDB = admin.database().ref("messages");
+   messageDB.push({
+     "name": "chris",
+     "text": "foo",
+   });
+
+
+   return;
+ });
